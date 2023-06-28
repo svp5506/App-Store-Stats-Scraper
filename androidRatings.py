4df2f625aaec92468c0ed87109c9e3fe4948bcfb
SP <svp5506@outlook.com>
2023-06-27 20:01:09 -0600

    iOS and Android Database Tables; Detail Date iOS

--- androidRatings.py
+++ androidRatings.py
@@ -2,46 +2,51 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 from datetime import datetime
+import sqlite3
+
+conn = sqlite3.connect("Database/app_store_stats.db")
+cursor = conn.cursor()
 
 urlAndroid = [
-    "https://play.google.com/store/apps/details?id=com.windstream.residential&hl=en_US&gl=US", #Windstream
-    "https://play.google.com/store/apps/details?id=com.altafiber.myaltafiber&hl=en_US&gl=US", #MyAltaFiber
-    "https://play.google.com/store/apps/details?id=com.spectrum.access&hl=en_US&gl=US", #SpectrumAcces
-    "https://play.google.com/store/apps/details?id=com.cox.android.mobileconnect&hl=en_US&gl=US", #CoxApp
-    "https://play.google.com/store/apps/details?id=com.vzw.hss.myverizon&hl=en_US&gl=US", #MyVerizon
-    "https://play.google.com/store/apps/details?id=com.mizmowireless.acctmgt&hl=en_US&gl=US", #MyCricket
-    "https://play.google.com/store/apps/details?id=com.dish.mydish&hl=en_US&gl=US", #MyDishAccount
-    "https://play.google.com/store/apps/details?id=com.tmobile.pr.mytmobile&hl=en_US&gl=US", #T-Mobile
-    "https://play.google.com/store/apps/details?id=com.xfinitymobile.myaccount&hl=en_US&gl=US", #XfinityMobile
-    "https://play.google.com/store/apps/details?id=com.twcable.twcnews&hl=en_US&gl=US", #SpectrumNews
-    "https://play.google.com/store/apps/details?id=com.brighthouse.mybhn&hl=en_US&gl=US", #MySpectrumApp
-    "https://play.google.com/store/apps/details?id=com.sprint.care&hl=en_US&gl=US", #MySprintMobile
-    "https://play.google.com/store/apps/details?id=com.verizon.myfios&hl=en_US&gl=US", #VerizonMyFios
-    "https://play.google.com/store/apps/details?id=com.centurylink.ctl_droid_wrap&hl=en_US&gl=US", #MyCenturyLink
-    "https://play.google.com/store/apps/details?id=com.visiblemobile.flagship&hl=en_US&gl=US", #VisibleMobile
-    "https://play.google.com/store/apps/details?id=com.charter.university&hl=en_US&gl=US", #SpectrumU
-    "https://play.google.com/store/apps/details?id=com.TWCableTV&hl=en_US&gl=US", #SpectrumTV
-    "https://play.google.com/store/apps/details?id=com.att.myWireless&hl=en_US&gl=US", #MyATT
-    "https://play.google.com/store/apps/details?id=com.twcsports.android&hl=en_US&gl=US", #SpectrumSportsNet
-    "https://play.google.com/store/apps/details?id=com.speechcycle.smartcare.android.mediacom&hl=en_US&gl=US", #MediacomConnect
-    "https://play.google.com/store/apps/details?id=com.nuance.nmc.sihome.metropcs&hl=en_US&gl=US", #MyMetro
-    "https://play.google.com/store/apps/details?id=com.frontier.selfserve&hl=en_US&gl=US", #MyFrontier
-    "https://play.google.com/store/apps/details?id=com.xfinity.digitalhome&hl=en_US&gl=US", #XfinityMyAccount
-    "https://play.google.com/store/apps/details?id=com.google.android.apps.fiber.myfiber&hl=en_US&gl=US", #GoogleFiber
-    "https://play.google.com/store/apps/details?id=com.viasat.cts.store.ViasatInternet&hl=en_US&gl=US", #MyViasat
-    "https://play.google.com/store/apps/details?id=agoc.mobile.account&hl=en_US&gl=US", #ArmStrong
-    "https://play.google.com/store/apps/details?id=com.astound.mobile.rcn&hl=en_US&gl=US", #RCN_Mobile #Astound_Mobile
-    "https://play.google.com/store/apps/details?id=com.hughesnet.HughesNetMobile&hl=en_US&gl=US", #HughesNet_Mobile
-    "https://play.google.com/store/apps/details?id=com.hawaiiantel.myht&hl=en_US&gl=US", #HT_MyAccount
-    "https://play.google.com/store/apps/details?id=com.midco.consumerapp&hl=en_US&gl=US", #MidcoMyAccount
-    "https://play.google.com/store/apps/details?id=com.optimum.lbsaopt&hl=en_US&gl=US", #OptimumSupport
-    "https://play.google.com/store/apps/details?id=com.uscc.myaccount&hl=en_US&gl=US", #US_Cellular_My_Account
-    "https://play.google.com/store/apps/details?id=com.sectv.mysectv&hl=en_US&gl=US", #MySecTV
-    "https://play.google.com/store/apps/details?id=com.alticeusa.alticeone.prod&hl=en_US&gl=US", #OptimumTV
-    "https://play.google.com/store/apps/details?id=com.tivo.android.abb&hl=en_US&gl=US", #BreezelineTV
-    "https://play.google.com/store/apps/details?id=com.brctv.myblueridge&hl=en_US&gl=US", #MyBlueRidge
-    "https://play.google.com/store/apps/details?id=com.buckeyebroadband.mybuckeye&hl=en_US&gl=US" #MyBuckeye
+    "https://play.google.com/store/apps/details?id=com.windstream.residential&hl=en_US&gl=US",  # Windstream
+    "https://play.google.com/store/apps/details?id=com.altafiber.myaltafiber&hl=en_US&gl=US",  # MyAltaFiber
+    "https://play.google.com/store/apps/details?id=com.spectrum.access&hl=en_US&gl=US",  # SpectrumAcces
+    "https://play.google.com/store/apps/details?id=com.cox.android.mobileconnect&hl=en_US&gl=US",  # CoxApp
+    "https://play.google.com/store/apps/details?id=com.vzw.hss.myverizon&hl=en_US&gl=US",  # MyVerizon
+    "https://play.google.com/store/apps/details?id=com.mizmowireless.acctmgt&hl=en_US&gl=US",  # MyCricket
+    "https://play.google.com/store/apps/details?id=com.dish.mydish&hl=en_US&gl=US",  # MyDishAccount
+    "https://play.google.com/store/apps/details?id=com.tmobile.pr.mytmobile&hl=en_US&gl=US",  # T-Mobile
+    "https://play.google.com/store/apps/details?id=com.xfinitymobile.myaccount&hl=en_US&gl=US",  # XfinityMobile
+    "https://play.google.com/store/apps/details?id=com.twcable.twcnews&hl=en_US&gl=US",  # SpectrumNews
+    "https://play.google.com/store/apps/details?id=com.brighthouse.mybhn&hl=en_US&gl=US",  # MySpectrumApp
+    "https://play.google.com/store/apps/details?id=com.sprint.care&hl=en_US&gl=US",  # MySprintMobile
+    "https://play.google.com/store/apps/details?id=com.verizon.myfios&hl=en_US&gl=US",  # VerizonMyFios
+    "https://play.google.com/store/apps/details?id=com.centurylink.ctl_droid_wrap&hl=en_US&gl=US",  # MyCenturyLink
+    "https://play.google.com/store/apps/details?id=com.visiblemobile.flagship&hl=en_US&gl=US",  # VisibleMobile
+    "https://play.google.com/store/apps/details?id=com.charter.university&hl=en_US&gl=US",  # SpectrumU
+    "https://play.google.com/store/apps/details?id=com.TWCableTV&hl=en_US&gl=US",  # SpectrumTV
+    "https://play.google.com/store/apps/details?id=com.att.myWireless&hl=en_US&gl=US",  # MyATT
+    "https://play.google.com/store/apps/details?id=com.twcsports.android&hl=en_US&gl=US",  # SpectrumSportsNet
+    "https://play.google.com/store/apps/details?id=com.speechcycle.smartcare.android.mediacom&hl=en_US&gl=US",  # MediacomConnect
+    "https://play.google.com/store/apps/details?id=com.nuance.nmc.sihome.metropcs&hl=en_US&gl=US",  # MyMetro
+    "https://play.google.com/store/apps/details?id=com.frontier.selfserve&hl=en_US&gl=US",  # MyFrontier
+    "https://play.google.com/store/apps/details?id=com.xfinity.digitalhome&hl=en_US&gl=US",  # XfinityMyAccount
+    "https://play.google.com/store/apps/details?id=com.google.android.apps.fiber.myfiber&hl=en_US&gl=US",  # GoogleFiber
+    "https://play.google.com/store/apps/details?id=com.viasat.cts.store.ViasatInternet&hl=en_US&gl=US",  # MyViasat
+    "https://play.google.com/store/apps/details?id=agoc.mobile.account&hl=en_US&gl=US",  # ArmStrong
+    "https://play.google.com/store/apps/details?id=com.astound.mobile.rcn&hl=en_US&gl=US",  # RCN_Mobile #Astound_Mobile
+    "https://play.google.com/store/apps/details?id=com.hughesnet.HughesNetMobile&hl=en_US&gl=US",  # HughesNet_Mobile
+    "https://play.google.com/store/apps/details?id=com.hawaiiantel.myht&hl=en_US&gl=US",  # HT_MyAccount
+    "https://play.google.com/store/apps/details?id=com.midco.consumerapp&hl=en_US&gl=US",  # MidcoMyAccount
+    "https://play.google.com/store/apps/details?id=com.optimum.lbsaopt&hl=en_US&gl=US",  # OptimumSupport
+    "https://play.google.com/store/apps/details?id=com.uscc.myaccount&hl=en_US&gl=US",  # US_Cellular_My_Account
+    "https://play.google.com/store/apps/details?id=com.sectv.mysectv&hl=en_US&gl=US",  # MySecTV
+    "https://play.google.com/store/apps/details?id=com.alticeusa.alticeone.prod&hl=en_US&gl=US",  # OptimumTV
+    "https://play.google.com/store/apps/details?id=com.tivo.android.abb&hl=en_US&gl=US",  # BreezelineTV
+    "https://play.google.com/store/apps/details?id=com.brctv.myblueridge&hl=en_US&gl=US",  # MyBlueRidge
+    "https://play.google.com/store/apps/details?id=com.buckeyebroadband.mybuckeye&hl=en_US&gl=US",  # MyBuckeye
 ]
+
 
 def process_url(link):
     resultAndroid = requests.get(link)
@@ -64,20 +69,89 @@ def process_url(link):
     divReviews = soupAndroid.find_all("div", {"class": "RutFAf wcB8se"})
     if divReviews:
         for i, div in enumerate(divReviews):
-            countReviewsRaw = div['title']
+            countReviewsRaw = div["title"]
             countReviews = countReviewsRaw.replace(",", "")
             temp_list[i + 1] = int(countReviews)
     return temp_list
+
 
 dataAndroid = []
 for link in urlAndroid:
     temp_list = process_url(link)
     dataAndroid.append(temp_list)
 
-dataAndroid = pd.DataFrame(dataAndroid, columns=['Date', '5 Star Reviews', '4 Star Reviews', '3 Star Reviews', '2 Star Reviews', '1 Star Reviews', 'Android App Rating', 'App Name'],
-                           index = ['GKW','MAF','SAEM','CA','MV','MC','MD','TM','XM','SNLS','MSA','MSP','VMF','MCL','VM','SU','STVA','ATT','SSLG','MDCM','MM','MFR','XF','GFBR','MVIA','ARM','ASTRCN','HUGH','HTMYA','MIDCO','OPTS','USCELL','SEC','OPTTV','BRE','BLUER','BUCK'])
+dataAndroid = pd.DataFrame(
+    dataAndroid,
+    columns=[
+        "Date",
+        "5 Star Reviews",
+        "4 Star Reviews",
+        "3 Star Reviews",
+        "2 Star Reviews",
+        "1 Star Reviews",
+        "Android App Rating",
+        "App Name",
+    ],
+    index=[
+        "GKW",
+        "MAF",
+        "SAEM",
+        "CA",
+        "MV",
+        "MC",
+        "MD",
+        "TM",
+        "XM",
+        "SNLS",
+        "MSA",
+        "MSP",
+        "VMF",
+        "MCL",
+        "VM",
+        "SU",
+        "STVA",
+        "ATT",
+        "SSLG",
+        "MDCM",
+        "MM",
+        "MFR",
+        "XF",
+        "GFBR",
+        "MVIA",
+        "ARM",
+        "ASTRCN",
+        "HUGH",
+        "HTMYA",
+        "MIDCO",
+        "OPTS",
+        "USCELL",
+        "SEC",
+        "OPTTV",
+        "BRE",
+        "BLUER",
+        "BUCK",
+    ],
+)
 
-dataAndroid['Android Total Reviews'] = dataAndroid.loc[:, '5 Star Reviews':'1 Star Reviews'].sum(1)
-dataAndroid = dataAndroid[['Date', 'App Name', 'Android App Rating', 'Android Total Reviews', '5 Star Reviews', '4 Star Reviews', '3 Star Reviews', '2 Star Reviews', '1 Star Reviews']]
+dataAndroid["Android Total Reviews"] = dataAndroid.loc[
+    :, "5 Star Reviews":"1 Star Reviews"
+].sum(1)
+dataAndroid = dataAndroid[
+    [
+        "Date",
+        "App Name",
+        "Android App Rating",
+        "Android Total Reviews",
+        "5 Star Reviews",
+        "4 Star Reviews",
+        "3 Star Reviews",
+        "2 Star Reviews",
+        "1 Star Reviews",
+    ]
+]
 
-dataAndroid.to_excel('AndroidRatings.xlsx')
+dataAndroid.to_excel("AndroidRatings.xlsx")
+
+dataAndroid.to_sql("dataAndroid", conn, if_exists="replace", index=False)
+conn.commit()
+conn.close()
--- iOSRatings.py
+++ iOSRatings.py
@@ -3,79 +3,131 @@ import requests
 import json
 import pandas as pd
 from datetime import datetime
+import sqlite3
 
-url = ["https://apps.apple.com/us/app/go-kinetic-by-windstream/id1342262959", #GoKinectByWindstream
-    "https://apps.apple.com/us/app/my-altafiber/id1245014739", #MyAltafiber
-    "https://apps.apple.com/us/app/spectrum-access-enabled-media/id1492182876", #SpectrumAccess
-    "https://apps.apple.com/us/app/cox-app/id415894489", #CoxApp
-    "https://apps.apple.com/us/app/my-verizon/id416023011", #MyVerizon
-    "https://apps.apple.com/us/app/mycricket-app/id626392754", #MyCricket
-    "https://apps.apple.com/us/app/mydish-account/id1123102087", #MyDishAccount
-    "https://apps.apple.com/us/app/t-mobile/id561625752", #T-Mobile
-    "https://apps.apple.com/us/app/xfinity-mobile/id1194745615", #XfinityMobile
-    "https://apps.apple.com/us/app/spectrum-news-local-stories/id740948885", #SpectrumNews
-    "https://apps.apple.com/us/app/my-spectrum/id942608209", #MySpectrumApp
-    "https://apps.apple.com/us/app/my-sprint-mobile/id491126018", #MySprintMobile
-    "https://apps.apple.com/us/app/verizon-my-fios/id476394945", #VerizonMyFios
-    "https://apps.apple.com/us/app/my-centurylink/id1015059570", #MyCenturyLink
-    "https://apps.apple.com/us/app/visible-mobile/id1367950045", #VisibleMobile
-    "https://apps.apple.com/us/app/spectrumu/id827887111", #SpectrumU
-    "https://apps.apple.com/us/app/spectrum-tv/id420455839", #SpectrumTV
-    "https://apps.apple.com/us/app/myat-t/id309172177", #MyATT
-    "https://apps.apple.com/us/app/spectrum-sportsnet-live-games/id563316826", #SpectrumSportsNet
-    "https://apps.apple.com/us/app/mediacomconnect/id527680234", #MediacomConnect
-    "https://apps.apple.com/us/app/mymetro/id1188830219", #MyMetro
-    "https://apps.apple.com/us/app/myfrontier/id978439794", #MyFrontier
-    "https://apps.apple.com/us/app/xfinity/id1178765645", #XfinityMyAccount
-    "https://apps.apple.com/us/app/google-fiber/id1063844759", #GoogleFiber
-    "https://apps.apple.com/us/app/my-viasat/id1341120640", #MyViasat
-    "https://apps.apple.com/us/app/armstrong/id636522134", #ArmStrong
-    "https://apps.apple.com/us/app/astound-mobile/id6449095899", #RCN_Mobile #AstoundMobile
-    "https://apps.apple.com/us/app/hughesnet-mobile/id1097579916", #HughesNet_Mobile
-    "https://apps.apple.com/us/app/ht-my-account/id1262963153", #HT_MyAccount
-    "https://apps.apple.com/us/app/midco-my-account/id1090421908", #MidcoMyAccount
-    "https://apps.apple.com/us/app/optimum-support/id1234273194", #OptimumSupport
-    "https://apps.apple.com/us/app/uscellular-my-account/id1228669675", #US_Cellular_My_Account
-    "https://apps.apple.com/us/app/se-next-powered-by-tivo/id1414862251", #MySecTV
-    "https://apps.apple.com/us/app/optimum-tv/id1296704509", #OptimumTV
-    "https://apps.apple.com/us/app/breezeline-tv/id787428890", #BreezelineTV
-    "https://apps.apple.com/us/app/my-blue-ridge/id1425929491", #MyBlueRidge
-    "https://apps.apple.com/us/app/mybuckeye/id1571340716" #MyBuckeye
+conn = sqlite3.connect("Database/app_store_stats.db")
+cursor = conn.cursor()
+
+url = [
+    "https://apps.apple.com/us/app/go-kinetic-by-windstream/id1342262959",  # GoKinectByWindstream
+    "https://apps.apple.com/us/app/my-altafiber/id1245014739",  # MyAltafiber
+    "https://apps.apple.com/us/app/spectrum-access-enabled-media/id1492182876",  # SpectrumAccess
+    "https://apps.apple.com/us/app/cox-app/id415894489",  # CoxApp
+    "https://apps.apple.com/us/app/my-verizon/id416023011",  # MyVerizon
+    "https://apps.apple.com/us/app/mycricket-app/id626392754",  # MyCricket
+    "https://apps.apple.com/us/app/mydish-account/id1123102087",  # MyDishAccount
+    "https://apps.apple.com/us/app/t-mobile/id561625752",  # T-Mobile
+    "https://apps.apple.com/us/app/xfinity-mobile/id1194745615",  # XfinityMobile
+    "https://apps.apple.com/us/app/spectrum-news-local-stories/id740948885",  # SpectrumNews
+    "https://apps.apple.com/us/app/my-spectrum/id942608209",  # MySpectrumApp
+    "https://apps.apple.com/us/app/my-sprint-mobile/id491126018",  # MySprintMobile
+    "https://apps.apple.com/us/app/verizon-my-fios/id476394945",  # VerizonMyFios
+    "https://apps.apple.com/us/app/my-centurylink/id1015059570",  # MyCenturyLink
+    "https://apps.apple.com/us/app/visible-mobile/id1367950045",  # VisibleMobile
+    "https://apps.apple.com/us/app/spectrumu/id827887111",  # SpectrumU
+    "https://apps.apple.com/us/app/spectrum-tv/id420455839",  # SpectrumTV
+    "https://apps.apple.com/us/app/myat-t/id309172177",  # MyATT
+    "https://apps.apple.com/us/app/spectrum-sportsnet-live-games/id563316826",  # SpectrumSportsNet
+    "https://apps.apple.com/us/app/mediacomconnect/id527680234",  # MediacomConnect
+    "https://apps.apple.com/us/app/mymetro/id1188830219",  # MyMetro
+    "https://apps.apple.com/us/app/myfrontier/id978439794",  # MyFrontier
+    "https://apps.apple.com/us/app/xfinity/id1178765645",  # XfinityMyAccount
+    "https://apps.apple.com/us/app/google-fiber/id1063844759",  # GoogleFiber
+    "https://apps.apple.com/us/app/my-viasat/id1341120640",  # MyViasat
+    "https://apps.apple.com/us/app/armstrong/id636522134",  # ArmStrong
+    "https://apps.apple.com/us/app/astound-mobile/id6449095899",  # RCN_Mobile #AstoundMobile
+    "https://apps.apple.com/us/app/hughesnet-mobile/id1097579916",  # HughesNet_Mobile
+    "https://apps.apple.com/us/app/ht-my-account/id1262963153",  # HT_MyAccount
+    "https://apps.apple.com/us/app/midco-my-account/id1090421908",  # MidcoMyAccount
+    "https://apps.apple.com/us/app/optimum-support/id1234273194",  # OptimumSupport
+    "https://apps.apple.com/us/app/uscellular-my-account/id1228669675",  # US_Cellular_My_Account
+    "https://apps.apple.com/us/app/se-next-powered-by-tivo/id1414862251",  # MySecTV
+    "https://apps.apple.com/us/app/optimum-tv/id1296704509",  # OptimumTV
+    "https://apps.apple.com/us/app/breezeline-tv/id787428890",  # BreezelineTV
+    "https://apps.apple.com/us/app/my-blue-ridge/id1425929491",  # MyBlueRidge
+    "https://apps.apple.com/us/app/mybuckeye/id1571340716",  # MyBuckeye
 ]
+
 
 def fetch_data(url):
     result = requests.get(url)
     soup = BeautifulSoup(result.content, "lxml")
-    rank_element = soup.find('a', {'class': 'inline-list__item'})
+    rank_element = soup.find("a", {"class": "inline-list__item"})
     rank = None
     if rank_element is not None:
         rank_text = rank_element.text.strip()
         rank_text = rank_text.split()[0]
-        rank = int(rank_text.replace(',', '').replace('#', ''))
+        rank = int(rank_text.replace(",", "").replace("#", ""))
     script = soup.find(type="application/ld+json").text.strip()
     data = json.loads(script)
 
     app_rating = None
     review_count = None
-    if 'aggregateRating' in data:
-        app_rating = data['aggregateRating'].get('ratingValue')
-        review_count = data['aggregateRating'].get('reviewCount')
+    if "aggregateRating" in data:
+        app_rating = data["aggregateRating"].get("ratingValue")
+        review_count = data["aggregateRating"].get("reviewCount")
 
     dataApp = {
-        "App Name": data.get('name'),
+        "App Name": data.get("name"),
         "iOS App Rating": app_rating,
         "iOS Total Reviews": review_count,
-        "iOS Rank": rank
+        "iOS Rank": rank,
     }
     return dataApp
+
 
 dataiOS = []
 for link in url:
     dataApp = fetch_data(link)
     dataiOS.append(dataApp)
 
-dataiOS = pd.DataFrame(dataiOS, 
-                       index = ['GKW','MAF','SAEM','CA','MV','MC','MD','TM','XM','SNLS','MSA','MSP','VMF','MCL','VM','SU','STVA','ATT','SSLG','MDCM','MM','MFR','XF','GFBR','MVIA','ARM','ASTRCN','HUGH','HTMYA','MIDCO','OPTS','USCELL','SEC','OPTTV','BRE','BLUER','BUCK'])
+dataiOS = pd.DataFrame(
+    dataiOS,
+    index=[
+        "GKW",
+        "MAF",
+        "SAEM",
+        "CA",
+        "MV",
+        "MC",
+        "MD",
+        "TM",
+        "XM",
+        "SNLS",
+        "MSA",
+        "MSP",
+        "VMF",
+        "MCL",
+        "VM",
+        "SU",
+        "STVA",
+        "ATT",
+        "SSLG",
+        "MDCM",
+        "MM",
+        "MFR",
+        "XF",
+        "GFBR",
+        "MVIA",
+        "ARM",
+        "ASTRCN",
+        "HUGH",
+        "HTMYA",
+        "MIDCO",
+        "OPTS",
+        "USCELL",
+        "SEC",
+        "OPTTV",
+        "BRE",
+        "BLUER",
+        "BUCK",
+    ],
+)
 now = datetime.now()
-dataiOS.insert(0, 'Date', now.strftime("%B %d, %Y"))
-dataiOS.to_excel('iOSratings.xlsx')
+dataiOS.insert(0, "Date", now.strftime("%B %d, %Y"))
+dataiOS.insert(0, "Detail Date", now.strftime("%Y-%m-%d %H:%M:%S"))
+dataiOS.to_excel("iOSratings.xlsx")
+
+dataiOS.to_sql("dataiOS", conn, if_exists="replace", index=False)
+conn.commit()
+conn.close()
